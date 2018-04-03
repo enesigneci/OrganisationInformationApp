67337b577c9d6e4b7e248fbd5a4f2e5f80acb333
Enes İğneci <enes.igneci@rdc.com.tr>
2018-04-03 16:06:04 +0300

    Splash

---
+++ app/src/main/java/com/enesigneci/dernek/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.enesigneci.dernek;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import com.enesigneci.dernek.user.MainActivity;
+
+public class SplashActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        ProgressBar splashProgressBar=findViewById(R.id.splash_progress_bar);
+        Handler splashHandler=new Handler();
+        splashHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        },1000);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+
+    }
+
+}
